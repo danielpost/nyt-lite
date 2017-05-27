c10ee1da5fc64a5f4833c35e5aa86a4ba4bce355
Daniel Post <daniel@danielpost.com>
2017-05-28 00:23:05 +0200

    Add image check to remove errors, add section filtering, get started on single articles

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview ArtistYear JavaScript Functionality
+ * @fileoverview The New York Times Lite
  * @author Daniel Post
  * @version 1.0
  */
@@ -11,13 +11,43 @@
      * App
      */
 
-    var apiURL = 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=39abdacf3adc4a93a23bf03ed0790397';
-
     var app = new Vue({
 
         el: '#app',
 
         data: {
+            sections: [
+                'all-sections',
+                'Arts',
+                'Automobiles',
+                'Books',
+                'Business Day',
+                'Education',
+                'Fashion & Style',
+                'Food',
+                'Health',
+                'Job Market',
+                'Magazine',
+                'Movies',
+                'Multimedia',
+                'Opinion',
+                'Public Editor',
+                'Real Estate',
+                'Science',
+                'Sports',
+                'Style',
+                'Sunday Review',
+                'T Magazine',
+                'Technology',
+                'The Upshot',
+                'Theater',
+                'Times Insider',
+                'Travel',
+                'U.S.',
+                'World',
+                'Your Money',
+            ],
+            currentSection: 'all-sections',
             articles: null
         },
 
@@ -25,20 +55,21 @@
             this.fetchData();
         },
 
+        watch: {
+            currentSection: 'fetchData'
+        },
+
         filters: {
-            truncate: function (v) {
-                var newline = v.indexOf('\n');
-                return newline > 0 ? v.slice(0, newline) : v;
-            },
-            formatDate: function (v) {
-                return v.replace(/T|Z/g, ' ');
+            formatSection: function (v) {
+                return v.replace('-', ' ').replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
             }
         },
 
         methods: {
             fetchData: function () {
                 var xhr = new XMLHttpRequest(),
-                    self = this;
+                    self = this,
+                    apiURL = 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/' + self.currentSection + '/1.json?api-key=39abdacf3adc4a93a23bf03ed0790397';
 
                 xhr.open('GET', apiURL);
 
@@ -46,15 +77,41 @@
                     var articles = JSON.parse(xhr.responseText).results;
 
                     for (var i in articles) {
-                        articles[i].image = {};
-                        articles[i].image.caption = articles[i].media[0].caption;
-                        articles[i].image.copyright = articles[i].media[0].copyright;
-                        articles[i].image.url = articles[i].media[0]['media-metadata'][3].url;
+                        if (articles[i].media) {
+                            articles[i].image = {};
+                            articles[i].image.caption = articles[i].media[0].caption || '';
+                            articles[i].image.copyright = articles[i].media[0].copyright || '';
+
+                            // all-sections has different image thumbnails so we need to pick the correct ones.
+                            var bigImage = self.currentSection === 'all-sections' ? 3 : 2,
+                                smallImage = self.currentSection === 'all-sections' ? 1 : 0;
+
+                            if (i % 3 === 0 ) {
+                                articles[i].image.url = articles[i].media[0]['media-metadata'][bigImage].url;
+                            } else {
+                                articles[i].image.url = articles[i].media[0]['media-metadata'][smallImage].url;
+                            }
+                        }
                     }
 
-                    console.log(articles[i]);
-
                     self.articles = articles;
+                };
+
+                xhr.send();
+            },
+            showArticle: function (title) {
+                var xhr = new XMLHttpRequest(),
+                    self = this,
+                    apiURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=39abdacf3adc4a93a23bf03ed0790397';
+
+                xhr.open('GET', apiURL + '&q=' + title);
+
+                xhr.onload = function () {
+                    var article = JSON.parse(xhr.responseText).response.docs[0];
+
+                    console.log(article);
+
+                    self.article = article;
                 };
 
                 xhr.send();
